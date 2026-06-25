6d0c4a0dae3240fd9434d16ee2a2c8b95f9d3201
watsonhaw <watsonhaw@hotmail.com>
2026-06-25 15:59:45 +0800

    refactor: 重构测试框架为 phpunit 10 (#1)
    * refactor(test): 重构测试为 phpunit10
    
    * chore: 更新 .gitignore
    
    * chore: composer.lcok 加入 git
    
    * fix: 修复 composer validate --strict
    
    * ci: 更新 CI
    
    * style: 格式化代码
    
    ---------
    
    Co-authored-by: watsonhaw5566 <348748267@qq.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,5 @@
 <?php
+
 define('STUB_DIR', realpath(__DIR__ . '/stub'));
+
+require __DIR__ . '/../vendor/autoload.php';
--- tests/feature/QueueTest.php
+++ tests/feature/QueueTest.php
@@ -1,5 +1,13 @@
 <?php
 
-it('queue', function () {
+namespace think\worker\tests\feature;
 
-});
+use PHPUnit\Framework\TestCase;
+
+class QueueTest extends TestCase
+{
+    public function test_queue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
