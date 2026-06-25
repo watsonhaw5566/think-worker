0c364b249164040258f3c30ca2e227a8f229e6ea
watsonhaw5566 <348748267@qq.com>
2026-06-25 18:42:26 +0800

    fix: 修复 Scan Find 存在的问题

--- src/watcher/Scan.php
+++ src/watcher/Scan.php
@@ -15,19 +15,29 @@ class Scan implements Driver
     public function __construct($directory, $exclude, $name)
     {
         $this->finder = new Finder();
-        $this->finder
-            ->files()
-            ->name($name)
-            ->in($directory)
-            ->exclude($exclude);
+        $this->finder->files()->name($name);
+
+        if (!empty($exclude)) {
+            $this->finder->exclude($exclude);
+        }
+
+        if (!empty($directory)) {
+            $this->finder->in($directory);
+        }
     }
 
     protected function findFiles()
     {
         $files = [];
-        /** @var SplFileInfo $f */
-        foreach ($this->finder as $f) {
-            $files[$f->getRealpath()] = $f->getMTime();
+        try {
+            /** @var SplFileInfo $f */
+            foreach ($this->finder as $f) {
+                $files[$f->getRealpath()] = $f->getMTime();
+            }
+        } catch (\Throwable $e) {
+            // Finder 在目录列表为空或不可访问时会抛异常
+            // 记录错误但不崩溃 watcher worker
+            error_log('[think-worker] Scan findFiles error: ' . $e->getMessage());
         }
         return $files;
     }
@@ -39,14 +49,35 @@ class Scan implements Driver
         Timer::add(2, function () use ($callback) {
             $files = $this->findFiles();
 
-            foreach ($files as $path => $time) {
-                if (empty($this->files[$path]) || $this->files[$path] != $time) {
-                    call_user_func($callback);
-                    break;
+            $changed = false;
+
+            // 快速路径：文件数量变化直接判定
+            if (count($files) !== count($this->files)) {
+                $changed = true;
+            } else {
+                // 检测新增或修改的文件
+                foreach ($files as $path => $time) {
+                    if (!isset($this->files[$path]) || $this->files[$path] !== $time) {
+                        $changed = true;
+                        break;
+                    }
+                }
+
+                // 检测删除的文件
+                if (!$changed) {
+                    foreach ($this->files as $path => $time) {
+                        if (!isset($files[$path])) {
+                            $changed = true;
+                            break;
+                        }
+                    }
                 }
             }
 
-            $this->files = $files;
+            if ($changed) {
+                $this->files = $files;
+                call_user_func($callback);
+            }
         });
     }
 }
